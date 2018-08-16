4f96c2507cf7df425615f74e90ae13137443d419
shineyilin <1477018503@qq.com>
2018-08-16 09:44:25 +0800

    first

---
+++ mapmethod/mapmethod.cpp
@@ -0,0 +1,29 @@
+/*
+ * mapmethod.cpp
+ *
+ *  Created on: 2018Äê7ÔÂ27ÈÕ
+ *      Author: admin
+ */
+
+#include <mapmethod/mapmethod.h>
+
+namespace std {
+
+mapmethod::mapmethod() {
+	// TODO Auto-generated constructor stub
+
+}
+
+void mapmethod::getPosDataList(FLOAT current_time)
+{
+    for (uint32 index = 0; index < 10; index++)
+    {
+
+    }
+}
+
+mapmethod::~mapmethod() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
